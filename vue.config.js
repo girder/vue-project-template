cc891685428365d08ae2bb1e0a47f1323eae58c1
Zach Mullen <zach.mullen@kitware.com>
2021-02-25 09:53:01 -0500

    Add comment about vuetify transpileDependencies

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ module.exports = {
   lintOnSave: false,
 
   transpileDependencies: [
+    // If babel ends up being required, this line is necessary. Without it,
+    //  it has no effect.
     'vuetify',
   ],
 };
