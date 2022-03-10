0bedc40fdc5778283d8d230b730d89a0f4931b95
Brandon Davis <git@subdavis.com>
2022-03-10 10:58:48 -0500

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  lintOnSave: false,
-
-  transpileDependencies: [
-    // If babel ends up being required, this line is necessary. Without it,
-    //  it has no effect.
-    'vuetify',
-  ],
-};
